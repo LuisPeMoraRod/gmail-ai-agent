a38dfa7d4239d799429dab4780c28049668a09b5
LuisPeMoraRod <luismorarod98@estudiantec.cr>
2025-06-27 13:21:13 -0600

    feat: abstract agent into a class

--- src/main.py
+++ src/main.py
@@ -1,35 +1,53 @@
 from langchain.chat_models import init_chat_model
 from langgraph.graph import StateGraph, START, END
 from langgraph.graph.message import add_messages
+from langgraph.checkpoint.memory import InMemorySaver
 from dotenv import load_dotenv
 from typing import Annotated
 from typing_extensions import TypedDict
-
-load_dotenv()
-model = init_chat_model("claude-3-5-sonnet-latest", model_provider="anthropic")
 
 
 class State(TypedDict):
     messages: Annotated[list, add_messages]
 
 
-def chatbot(state: State):
-    return {"messages": [model.invoke(state["messages"])]}
+class Agent:
+    def __init__(self, model_name: str, model_provider: str):
+        self.model = init_chat_model(model_name, model_provider=model_provider)
+        self.graph = self.build_graph()
+
+    def build_graph(self) -> StateGraph:
+        graph_builder = StateGraph(State)
+
+        graph_builder.add_node("chatbot", self.chatbot)
+        graph_builder.add_edge(START, "chatbot")
+        graph_builder.add_edge("chatbot", END)
+
+        checkpointer = InMemorySaver()
+        graph = graph_builder.compile(checkpointer=checkpointer)
+        return graph
+
+    def chatbot(self, state: State):
+        return {"messages": [self.model.invoke(state["messages"])]}
+
+    def run(self):
+        while True:
+            user_input = input("Enter a message: ")
+            state = self.graph.invoke(
+                {"messages": [{"role": "user", "content": user_input}]},
+                {"configurable": {"thread_id": "1"}},
+            )
+
+            print(state["messages"][-1].content)
 
 
 def main():
-    graph_builder = StateGraph(State)
+    load_dotenv()
 
-    graph_builder.add_node("chatbot", chatbot)
-    graph_builder.add_edge(START, "chatbot")
-    graph_builder.add_edge("chatbot", END)
-
-    graph = graph_builder.compile()
-
-    user_input = input("Enter a message: ")
-    state = graph.invoke({"messages": [{"role": "user", "content": user_input}]})
-
-    print(state["messages"][-1].content)
+    model_name = "claude-3-5-sonnet-latest"
+    model_provider = "anthropic"
+    agent = Agent(model_name, model_provider)
+    agent.run()
 
 
 if __name__ == "__main__":
